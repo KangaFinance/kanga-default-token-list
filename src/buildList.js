05ee422f2d4837692123ce5004b2f46c72cce092
John Whitton <john@johnwhitton.com>
2021-08-30 10:25:00 -0700

    Add in all chains

--- src/buildList.js
+++ src/buildList.js
@@ -1,13 +1,32 @@
 const { version } = require("../package.json");
-/*const mainnet = require("./tokens/mainnet.json");
+const mainnet = require("./tokens/mainnet.json");
 const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");*/
-const bscMainnet = require("./tokens/bsc-mainnet.json");
+const kovan = require("./tokens/kovan.json");
+const fantom = require("./tokens/fantom.json");
+const fantomTestnet = require("./tokens/fantom-testnet.json");
+const matic = require("./tokens/matic.json");
+const maticTestnet = require("./tokens/matic-testnet.json");
+const xdai = require("./tokens/xdai.json");
+
+const bsc = require("./tokens/bsc-mainnet.json");
 const bscTestnet = require("./tokens/bsc-testnet.json");
-const harmonyMainnet = require("./tokens/harmony-mainnet.json");
+
+const moonbase = require("./tokens/moonbase.json");
+const avalanche = require("./tokens/avalanche.json");
+const fuji = require("./tokens/fuji.json");
+const heco = require("./tokens/heco.json");
+const hecoTestnet = require("./tokens/heco-testnet.json");
+
+const harmony = require("./tokens/harmony-mainnet.json");
 const harmonyTestnet = require("./tokens/harmony-testnet.json");
+
+const okex = require("./tokens/okex.json");
+const okexTestnet = require("./tokens/okex-testnet.json");
+// const arbitrum = require("../tokens/arbitrum.json");
+const celo = require("./tokens/celo.json");
+const palm = require("./tokens/palm.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -22,7 +41,31 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://kanga.finance/images/KANGAROO-FAST-512.png",
     keywords: ["kanga", "default"],
-    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
+    tokens: [
+      ...mainnet,
+      ...ropsten,
+      ...goerli,
+      ...kovan,
+      ...rinkeby,
+      ...fantom,
+      ...fantomTestnet,
+      ...matic,
+      ...maticTestnet,
+      ...xdai,
+      ...bsc,
+      ...bscTestnet,
+      ...moonbase,
+      ...avalanche,
+      ...fuji,
+      ...heco,
+      ...hecoTestnet,
+      ...harmony,
+      ...harmonyTestnet,
+      ...okex,
+      ...okexTestnet,
+      // ...arbitrum,
+      ...celo,
+      ...palm]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
